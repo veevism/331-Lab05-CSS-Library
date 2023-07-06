94145aa7d39ccd21d59642b2fa4e52c3afec588f
sorawee <veevisorawee@gmail.com>
2023-07-07 00:01:42 +0700

    step 2.3

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import { useMessageStore } from './stores/message';
+import { storeToRefs } from 'pinia';
+
+const store = useMessageStore()
+const { message } = storeToRefs(store)
+
+</script>
+
+<template>
+  <header>
+    <div id="flashMessage" v-if="message">
+      <h4>{{ message }}</h4>
+    </div>
+    <nav>
+      <RouterLink :to="{ name: 'event-list' }">Home</RouterLink>
+      <RouterLink :to="{ name: 'about' }">About</RouterLink>
+    </nav>
+  </header>
+  <RouterView />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+h4 {
+  font-size: 20px;
+}
+
+@keyframes yellowfade {
+  from {
+    background: yellow;
+  }
+
+  to {
+    background: transparent;
+  }
+}
+
+#flashMessage {
+  animation: yellowfade 3s ease-in-out;
+}
+</style>
---
+++ src/views/EventListView.vue
@@ -0,0 +1,91 @@
+<script setup lang="ts">
+import EventCard from '../components/EventCard.vue'
+import type { EventItem } from '@/type'
+import { ref, watchEffect, type Ref, computed } from 'vue'
+import EventService from '@/services/EventService'
+import type { AxiosResponse } from 'axios';
+import { useRouter } from 'vue-router'
+import NProgress from 'nprogress'
+import { onBeforeRouteUpdate } from 'vue-router';
+
+const events: Ref<Array<EventItem>> = ref([])
+
+const router = useRouter()
+
+const totalEvent = ref<number>(0)
+
+
+const props = defineProps({
+  page: {
+    type: Number,
+    required: true
+  }
+  ,
+  limit: {
+    type: Number,
+    required: true
+  }
+})
+
+EventService.getEvent(3, props.page).then((response: AxiosResponse<EventItem[]>) => {
+  events.value = response.data
+  totalEvent.value = response.headers['x-total-count']
+}).catch(() => {
+  router.push({ name: 'NetworkError' })
+})
+
+onBeforeRouteUpdate((to, from, next) => {
+  const toPage = Number(to.query.page)
+  EventService.getEvent(3, toPage).then((response: AxiosResponse<EventItem[]>) => {
+    events.value = response.data
+    totalEvent.value = response.headers['x-total-count']
+    next()
+  }).catch(() => {
+    next({ name: 'NetworkError' })
+  })
+})
+
+const hasNextPage = computed(() => {
+  const totalPages = Math.ceil(totalEvent.value / 3)
+  return props.page.valueOf() < totalPages
+})
+
+</script> 
+
+<template>
+  <main class="flex flex-col items-center">
+    <EventCard v-for="event in events" :key="event.id" :event="event"></EventCard>
+    <div class="pagination">
+      <RouterLink :to="{ name: 'event-list', query: { page: page - 1, limit: limit } }" rel="prev" v-if="page != 1"
+        id="page-prev"> Prev
+        Page
+      </RouterLink>
+      <RouterLink :to="{ name: 'event-list', query: { page: page + 1, limit: limit } }" rel="next" v-if="hasNextPage"
+        id="page-next">
+        Next Page
+      </RouterLink>
+    </div>
+
+  </main>
+</template>
+
+<style scoped>
+.pagination {
+  display: flex;
+  width: 290px;
+}
+
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+
+#page-next {
+  text-align: right;
+}
+</style>
